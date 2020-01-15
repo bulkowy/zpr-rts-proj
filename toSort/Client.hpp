c9f0f8dd8ef8c47d84677a696832c45adb386cde
Bulkowy <bulexxd@gmail.com>
2020-01-15 13:06:15 +0100

    comments more

--- toSort/Client.hpp
+++ toSort/Client.hpp
@@ -8,9 +8,20 @@
 
 namespace client
 {
+/**
+ * @brief Class client that defines threads used for communication with main server
+ * 
+ * Client will talk either with main server or with server's game thread
+ * when such thread will appear and Client will joing the game
+ */
 class Client
 {
 public:
+    /**
+     * @brief Base default constructor for Client class
+     * 
+     * Initializes main members for later usage
+     */
     Client();
 
     Client(const Client&) = delete;
@@ -18,33 +29,78 @@ public:
 
     virtual ~Client();
 
+    /**
+     * @brief Main client loop
+     * 
+     * Defines loop that process packets from server and prepare ones from client to be sent
+     */
     void run();
+
+    /**
+     * @brief Method used for stopping Client and its threads
+     */
     void stop();
+
+    /**
+     * @brief Wait for all threads to end
+     */
     void wait() { clientThread_.wait(); }
 
+    /**
+     * @brief Get the Client ID
+     * 
+     * @return int 
+     */
     int getClientID() const { return clientID_; }
-    int setClientID(int clientID) { clientID_ = clientID; }
 
+    /**
+     * @brief Set the Client ID
+     * 
+     * @param clientID - new client ID
+     */
+    void setClientID(int clientID) { clientID_ = clientID; }
+
+    // tmp?
     bool getStop() const { return stop_; }
     
+    /**
+     * @brief Send given packet to the Server
+     * 
+     * @param packet - packet to be sent to the server
+     */
     void send(sf::Packet&);
+
+    // tmp
     void poolMove(int move);
 
+    /**
+     * @brief Secure method to disconnect from server
+     */
     void disconnect();
 
-    sf::TcpSocket socket_;
+    sf::TcpSocket socket_; /**< socket used in communication with server */
 
 private:
     typedef std::unique_ptr<entity::Entity> EntityPtr; 
-    sf::Thread clientThread_;
 
+    sf::Thread clientThread_; /**< thread for client's main communication loop */
+
+    /**
+     * @brief Method to parse updates on client and send them to server
+     */
     void tick();
+
+    /**
+     * @brief Method deserializing incoming packet from server
+     * 
+     * @param packet - packet that came from server
+     */
     void handleServerPacket(sf::Packet&);
 
-    int clientID_;
-    unsigned int port_;
-    bool stop_;
-    bool isConnected_;
+    int clientID_;      /**< client's id */
+    unsigned int port_; /**< port of server */
+    bool stop_;         /**< is client stopped */
+    bool isConnected_;  /**< is client connected */
 
     enum NextMove {
         NOMOVE, UP, DOWN, LEFT, RIGHT
@@ -53,8 +109,8 @@ private:
     NextMove nm_;
 
     sf::Int32 ownEntityID_;
-    std::vector<entity::Entity> allEntities_;
-    sf::Time clientTimeout_;
+    std::vector<entity::Entity> allEntities_; /**< entities in game */
+    sf::Time clientTimeout_; /**< time for client to timeout from server */
 
 };
 }
